faaadb56ad9b55391aeafe88d5e3b16914376388
bienfait shomari <bienfaitshm@gmail.com>
2025-02-10 20:52:21 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>Project vente pos (by Bienfait shomari)</h1>
+    </div>
+  );
+}
