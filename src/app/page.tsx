e357758591d9840da10f4917735d63c6e74e9ba0
bienfait shomari <bienfaitshm@gmail.com>
2025-02-11 19:18:48 +0100

    feat: remove content in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import { signIn } from "@/auth";
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-screen flex justify-center items-center">
       <h1>Project vente pos (by Bienfait shomari)</h1>
-      <div>
-        <form
-          action={async (formData) => {
-            "use server";
-            await signIn("resend", formData);
-          }}
-        >
-          <input type="text" name="email" placeholder="Email" />
-          <button type="submit">Signin with Resend</button>
-        </form>
-      </div>
     </div>
   );
 }
