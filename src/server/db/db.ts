479bf4b18d61f789bdbc7defb30f0321f36e05bf
bienfait shomari <bienfaitshm@gmail.com>
2025-02-11 16:42:16 +0100

    db: add config

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,3 +1,19 @@
+// src/db.ts
 import { drizzle } from "drizzle-orm/neon-http";
+import { neon, neonConfig } from "@neondatabase/serverless";
+// import { config } from "dotenv";
 
-export const db = drizzle(process.env.DATABASE_URL);
+// config({ path: ".env" }); // or .env.local
+
+import ws from "ws";
+neonConfig.webSocketConstructor = ws;
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
+
+// const sql = neon(process.env.DATABASE_URL);
+
+// export const db = drizzle({ client: sql });
