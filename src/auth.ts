76c92f94ef34df692aa54344302c2d9a2a042a44
bienfait shomari <bienfaitshm@gmail.com>
2025-02-11 07:56:51 +0100

    feat: add database config

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,28 @@
 import NextAuth from "next-auth";
 import Resend from "next-auth/providers/resend";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import {
+  accounts,
+  sessions,
+  users,
+  verificationTokens,
+} from "./server/db/schemas";
+import { db } from "./server/db/db";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Resend],
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    Resend({
+      apiKey: process.env.RESEND_API_KEY,
+      from: "vente-pos@company.com",
+    }),
+  ],
 });
