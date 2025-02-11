f83df7ee8db9e557692fc1136f1f2cce546ab659
bienfait shomari <bienfaitshm@gmail.com>
2025-02-11 20:05:16 +0100

    feat: add hash password function

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,4 +1,4 @@
-import { asc, between, count, eq, getTableColumns, sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { InsertUser, users as userTable } from "./schemas";
 
