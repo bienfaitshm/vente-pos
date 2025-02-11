745f05e4d5b1d3c2666506a1279378ce0d8f4792
bienfait shomari <bienfaitshm@gmail.com>
2025-02-11 18:51:02 +0100

    feat: add mutation of signin

--- src/hooks/mutations.ts
+++ src/hooks/mutations.ts
@@ -1,11 +1,19 @@
-import { loginUser } from "@/server/actions/account";
+import { loginUser, signinUser } from "@/server/actions/account";
 import { useMutation } from "@tanstack/react-query";
-import type { LoginCredential } from "@/lib/schemas";
+import type { LoginCredential, RegistrationCredential } from "@/lib/schemas";
 
 export function useLoginUser() {
   return useMutation({
     networkMode: "always",
     mutationKey: ["LOGIN"],
     mutationFn: (value: LoginCredential) => loginUser(value),
+  });
+}
+
+export function useSigninUser() {
+  return useMutation({
+    networkMode: "always",
+    mutationKey: ["SIGNIN"],
+    mutationFn: (value: RegistrationCredential) => signinUser(value),
   });
 }
--- src/server/actions/account.ts
+++ src/server/actions/account.ts
@@ -1,10 +1,25 @@
 "use server";
 
-import { LoginCredentialSchemas } from "@/lib/schemas";
+import {
+  LoginCredentialSchemas,
+  RegistrationCredentialSchemas,
+} from "@/lib/schemas";
 import { actionClient } from "./base";
 
 export const loginUser = actionClient
   .schema(LoginCredentialSchemas)
+  .action(async ({ parsedInput: { username, password } }) => {
+    if (username === "johndoe" && password === "123456") {
+      return {
+        success: "Successfully logged in",
+      };
+    }
+
+    return { failure: "Incorrect credentials" };
+  });
+
+export const signinUser = actionClient
+  .schema(RegistrationCredentialSchemas)
   .action(async ({ parsedInput: { username, password } }) => {
     if (username === "johndoe" && password === "123456") {
       return {
