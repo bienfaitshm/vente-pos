ad9b574187f98ba2bd57234a3eca583260d5c469
bienfait shomari <bienfaitshm@gmail.com>
2025-02-12 18:02:58 +0100

    feat: add auth type module

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { CredentialsSignin } from "next-auth";
+import NextAuth, { CredentialsSignin, DefaultSession } from "next-auth";
 import Resend from "next-auth/providers/resend";
 import Credentials from "next-auth/providers/credentials";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
@@ -14,7 +14,27 @@ class InvalidLoginError extends CredentialsSignin {
   code = "Invalid identifier or password";
 }
 
+declare module "next-auth" {
+  interface User {
+    id?: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    isAdmin?: boolean;
+    username?: string;
+  }
+  interface Session {
+    user: {
+      isAdmin: boolean;
+      username: string;
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  pages: {
+    signIn: "/auth/signin",
+  },
   adapter: DrizzleAdapter(db, {
     usersTable: users,
     accountsTable: accounts,
@@ -23,6 +43,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   }),
   session: {
     strategy: "jwt",
+  },
+
+  callbacks: {
+    session({ session, token, user }) {
+      // `session.user.address` is now a valid property, and will be type-checked
+      // in places like `useSession().data.user` or `auth().user`
+      return {
+        ...session,
+        user,
+      };
+    },
   },
   providers: [
     Resend({
@@ -37,6 +68,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       async authorize({ username, password }) {
         // const user = await db("users").where({ username }).first();
         throw new InvalidLoginError();
+        return null;
       },
     }),
   ],
