3cb26249b20059a760ea66da3a94ab277b5d4438
bienfait shomari <bienfaitshm@gmail.com>
2025-02-13 07:55:58 +0100

    feat: add queries check if user exist

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -2,12 +2,52 @@ import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { InsertUser, SelectUser, users as userTable } from "./schemas";
 
+/**
+ *
+ * @param data
+ */
 export async function createUser(data: InsertUser) {
   await db.insert(userTable).values(data);
 }
 
+/**
+ *
+ * @param email
+ * @returns
+ */
+export async function isEmailExist(email: string): Promise<boolean> {
+  return !!(await db
+    .select({ id: userTable.id })
+    .from(userTable)
+    .where(eq(userTable.email, email))
+    .limit(1));
+}
+
+/**
+ *
+ * @param username
+ * @returns
+ */
+export async function isUsernameExist(username: string): Promise<boolean> {
+  return !!(await db
+    .select({ id: userTable.id })
+    .from(userTable)
+    .where(eq(userTable.username, username))
+    .limit(1));
+}
+
+/**
+ *
+ * @param email
+ * @returns
+ */
 export async function getUserByEmail(email: string) {
-  return await db.select().from(userTable).where(eq(userTable.email, email));
+  const users = await db
+    .select()
+    .from(userTable)
+    .where(eq(userTable.email, email))
+    .limit(1);
+  return users[0];
 }
 
 /**
