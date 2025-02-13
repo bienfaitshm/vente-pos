5fc72327f0dddad9c6fa2a9373000d371ac58409
bienfait shomari <bienfaitshm@gmail.com>
2025-02-13 19:11:06 +0100

    feat: add select combox

---
+++ src/components/fields/select-combobox.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface SelectCombobox {
+  selections?: { value: number | string; label: string }[];
+  placeholder: string;
+  emptyMessage?: string;
+  value?: string | number;
+  onChangeValue?(value: number | string): void;
+}
+export const SelectCombobox: React.FC<SelectCombobox> = ({
+  selections = [],
+  emptyMessage = "Not found...",
+  placeholder,
+  onChangeValue,
+  value,
+}) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOnSelect = (currentValue: string) => {
+    onChangeValue?.(currentValue === value ? "" : currentValue);
+    setOpen(false);
+  };
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? selections.find((item) => item.value === value)?.label
+            : placeholder}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder={placeholder} />
+          <CommandList>
+            <CommandEmpty>{emptyMessage}</CommandEmpty>
+            <CommandGroup>
+              {selections.map((item) => (
+                <CommandItem
+                  key={item.value}
+                  value={item.value.toString()}
+                  onSelect={handleOnSelect}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === item.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {item.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
