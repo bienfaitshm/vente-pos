2132c2e6d266544cf26585e814881736156555b9
bienfait shomari <bienfaitshm@gmail.com>
2025-02-13 10:18:53 +0100

    fix: comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ export { auth as middleware } from "@/auth";
 export const config = {
   matcher: [
     // "/((?!api|_next/static|_next/image|favicon.ico).*)",
-    "/dashboard/:path*",
+    // "/dashboard/:path*",
     "/",
   ],
 };
