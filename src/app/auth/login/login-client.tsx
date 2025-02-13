09ae961fc336951d93287b5d658ed8aa1f8d4077
bienfait shomari <bienfaitshm@gmail.com>
2025-02-13 07:23:32 +0100

    style: remove unused code

--- src/app/auth/login/login-client.tsx
+++ src/app/auth/login/login-client.tsx
@@ -2,21 +2,9 @@
 import { ButtonLoader } from "@/components/button-loader";
 import { LoginForm } from "@/components/forms/login-form";
 import { useLoginUser } from "@/hooks/mutations";
-import type { LoginCredential } from "@/lib/schemas";
 
 export const LoginClient = () => {
   const mutation = useLoginUser();
-  // const handlerSubmit = async (value: LoginCredential) => {
-  //  const d =  await mutation.mutateAsync(value);
-  //   mutation.mutate(value, {
-  //     onSuccess: (value) => {
-  //       console.log("Login success", value);
-  //     },
-  //     onError: (error) => {
-  //       console.log("Login failed", error);
-  //     },
-  //   });
-  // };
   return (
     <LoginForm onSubmit={mutation.mutateAsync}>
       <ButtonLoader isLoading={mutation.isPending} loadingText="Login...">
