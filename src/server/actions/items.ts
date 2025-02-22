2d96766bac69039f1e3710a69338af50bdbc4735
bienfait shomari <bienfaitshm@gmail.com>
2025-02-22 04:06:49 +0100

    feat: add queries to actions

--- src/server/actions/items.ts
+++ src/server/actions/items.ts
@@ -51,19 +51,44 @@ export const deleteCategory = actionClient
 
 export const getProducts = actionClient
   .schema(ProductSchemas)
-  .action(async ({ parsedInput: {} }) => {});
+  .action(async ({ parsedInput: {} }) => {
+    return await queries.getProducts();
+  });
 
 export const createProduct = actionClient
   .schema(ProductSchemas)
-  .action(async ({ parsedInput: {} }) => {});
+  .action(async ({ parsedInput: values }) => {
+    const data = await queries.createProduct({
+      ...values,
+      category: values.category as number,
+    });
+    revalidatePath("/");
+    return data;
+  });
 
 export const updateProduct = actionClient
   .schema(ProductSchemas)
-  .action(async ({ parsedInput: {} }) => {});
+  .action(async ({ parsedInput: { id, category, name, price, quantity } }) => {
+    const data = await queries.updateProduct({
+      id: id as number,
+      category: category as number,
+      name,
+      price,
+      quantity,
+    });
+    revalidatePath("/");
+    return data;
+  });
 
 export const deleteProduct = actionClient
   .schema(IdObjectSchems)
-  .action(async ({ parsedInput: {} }) => {});
+  .action(async ({ parsedInput: { id } }) => {
+    const data = await queries.deleteProduct({
+      id: id as number,
+    });
+    revalidatePath("/");
+    return data;
+  });
 
 export const changeProductQuantity = actionClient
   .schema(ProductQuantitySchemas)
