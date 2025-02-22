b58adf86d6ab3df4b2f1e059fd3c529e7cfc3705
bienfait shomari <bienfaitshm@gmail.com>
2025-02-22 03:56:13 +0100

    feat: add product columns

--- src/components/tables/columns/index.tsx
+++ src/components/tables/columns/index.tsx
@@ -11,6 +11,13 @@ export type ColumnCategoryType = {
   name: string;
 };
 
+export type ColumnProductType = {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
 export type StudentPayementFraisType = {
   id: number | string;
   fullName: string;
@@ -55,6 +62,95 @@ export function getCategoryColumn(
             {/* {label && <Badge variant="outline">{label.label}</Badge>} */}
             <span className="max-w-48 truncate font-medium">
               {row.getValue("name")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      id: "actions",
+      cell({ row }) {
+        return <EditDeleteCellAction row={row} {...params?.actions} />;
+      },
+    },
+  ];
+}
+
+/**
+ *
+ * @param params
+ * @returns
+ */
+export function getProductColumns(
+  params?: CategoryColumnParams<ColumnCategoryType>
+): ColumnDef<ColumnCategoryType>[] {
+  return [
+    {
+      accessorKey: "id",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="ID" />
+      ),
+      cell: ({ row }) => (
+        <div className="max-w-10 w-5 truncate">{row.getValue("id")}</div>
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "name",
+      enableHiding: true,
+      enableResizing: true,
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Nom" />
+      ),
+      cell: ({ row }): React.ReactNode => {
+        // const label = labels.find((label) => label.value === row.original.label)
+
+        return (
+          <div className="flex space-x-2">
+            {/* {label && <Badge variant="outline">{label.label}</Badge>} */}
+            <span className="max-w-48 truncate font-medium">
+              {row.getValue("name")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "price",
+      enableHiding: true,
+      enableResizing: true,
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Prix" />
+      ),
+      cell: ({ row }): React.ReactNode => {
+        // const label = labels.find((label) => label.value === row.original.label)
+
+        return (
+          <div className="flex space-x-2">
+            {/* {label && <Badge variant="outline">{label.label}</Badge>} */}
+            <span className="max-w-48 truncate font-medium">
+              {row.getValue("price")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "quantity",
+      enableHiding: true,
+      enableResizing: true,
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="En Stock" />
+      ),
+      cell: ({ row }): React.ReactNode => {
+        // const label = labels.find((label) => label.value === row.original.label)
+
+        return (
+          <div className="flex space-x-2">
+            {/* {label && <Badge variant="outline">{label.label}</Badge>} */}
+            <span className="max-w-48 truncate font-medium">
+              {row.getValue("quantity")}
             </span>
           </div>
         );
