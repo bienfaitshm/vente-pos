4130721987e5ae983fb36ab644e33c72c7d0e3e5
bienfait shomari <bienfaitshm@gmail.com>
2025-03-07 05:30:31 +0200

    feat: change lenght of caracters of id

--- src/server/db/schemas/base.ts
+++ src/server/db/schemas/base.ts
@@ -3,7 +3,7 @@ import ShortUniqueId from "short-unique-id";
 
 const shordId = new ShortUniqueId({ length: 5 });
 export const commonFieldTable = {
-  id: varchar("id")
+  id: varchar("id", { length: 10 })
     .primaryKey()
     .$defaultFn(() => shordId.randomUUID()),
   updatedAt: timestamp("updated_at", { mode: "date" }),
