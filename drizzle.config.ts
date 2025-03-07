12d64b1155c78c5ce583452a6106e01619928854
bienfait shomari <bienfaitshm@gmail.com>
2025-03-07 05:28:58 +0200

    feat: change schemas drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./src/server/db/migrations",
-  schema: "./src/server/db/schemas.ts",
+  schema: "./src/server/db/schemas",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
