bca3022b945732f71a3d2c8c530483785fea7833
bienfait shomari <bienfaitshm@gmail.com>
2025-03-14 10:51:48 +0200

    feat: ajouter des schémas pour les activités de stock, les clients, les commandes et les points de vente, et améliorer la validation des produits

--- src/lib/schemas/activities.ts
+++ src/lib/schemas/activities.ts
@@ -1,12 +1,132 @@
 import { z } from "zod";
-import { IDSchemas } from "./base";
+import { IDSchemas, NoEmptyStringSchemas } from "./base";
 
+/**
+ * Schema definition for stock-related activities.
+ *
+ * This schema validates the structure of stock activity objects, ensuring
+ * that they conform to the expected format and constraints.
+ *
+ * Properties:
+ * - `action`: Specifies the type of stock action. It can either be "REMOVE" or "ADD".
+ *   Defaults to "ADD".
+ * - `quantity`: The quantity of stock involved in the action. Must be a number
+ *   with a minimum value of -1.
+ * - `productId`: The unique identifier for the product involved in the stock action.
+ *   Validated using the `IDSchemas` schema.
+ * - `posId`: The unique identifier for the point of sale (POS) where the stock
+ *   action is taking place. Validated using the `IDSchemas` schema.
+ * - `sellerId`: The unique identifier for the seller performing the stock action.
+ *   Validated using the `IDSchemas` schema.
+ */
 export const StockSchemas = z.object({
-  action: z.enum(["SUB", "ADD"]).default("ADD"),
+  action: z.enum(["REMOVE", "ADD"]).default("ADD"),
   quantity: z.coerce.number().min(-1),
-  product: IDSchemas,
-  pos: IDSchemas,
-  saler: IDSchemas,
+  productId: IDSchemas,
+  posId: IDSchemas,
+  sellerId: IDSchemas,
 });
 
 export type Stock = z.infer<typeof StockSchemas>;
+
+/**
+ * Schema definition for a customer object.
+ *
+ * This schema validates the structure of a customer object, ensuring that:
+ * - `name` is a non-empty string (validated by `NoEmptyStringSchemas`).
+ * - `phoneNumber` is an optional string.
+ * - `address` is an optional string.
+ *
+ * @constant
+ */
+export const CustomerSchemas = z.object({
+  name: NoEmptyStringSchemas,
+  phoneNumber: z.string().optional(),
+  address: z.string().optional(),
+});
+
+/**
+ * Type for Customer
+ */
+export type Customer = z.infer<typeof CustomerSchemas>;
+
+/**
+ * Schema for Command Item
+ */
+export const DetailOrderSchemas = z.object({
+  quantity: z.coerce.number().min(1),
+  productId: IDSchemas,
+  productName: z.string().nonempty(),
+  unitPrice: z.coerce.number(),
+  commission: z.coerce.number(),
+});
+
+/**
+ * Type for Command Item
+ */
+export type DetailOrder = z.infer<typeof DetailOrderSchemas>;
+
+/**
+ * Represents the schema for an order.
+ *
+ * This schema validates the structure of an order object, including the customer ID,
+ * order details, and the saler ID. It ensures that the order contains at least one
+ * product and that no duplicate products are allowed.
+ */
+export const OrderSchemas = z.object({
+  /**
+   * The ID of the customer placing the order.
+   *
+   * This field is validated using the `IDSchemas` schema.
+   */
+  customerID: IDSchemas,
+
+  /**
+   * An array of order details.
+   *
+   * - Must contain at least one product.
+   * - Ensures that no duplicate products (based on `productId`) are present.
+   *
+   * If duplicate products are detected, a custom validation error with the message
+   * "Produit dupliqué non autorisé." will be raised.
+   */
+  orderDetails: z
+    .array(DetailOrderSchemas)
+    .min(1, { message: "Minimum de produit requis" })
+    .superRefine((val, ctx) => {
+      if (val.length !== new Set(val.map((item) => item.productId)).size) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Produit dupliqué non autorisé.",
+        });
+      }
+    }),
+
+  /**
+   * The ID of the saler associated with the order.
+   *
+   * This field is validated using the `IDSchemas` schema.
+   */
+  saler: IDSchemas,
+});
+
+/**
+ * Type for Order
+ */
+export type Order = z.infer<typeof OrderSchemas>;
+
+/**
+ * Schema for Point of Sale
+ */
+export const PointOfSaleSchemas = z.object({
+  name: NoEmptyStringSchemas,
+  description: z.string().optional(),
+  address: NoEmptyStringSchemas,
+  phoneNumber: NoEmptyStringSchemas,
+  statut: z.enum(["OPEN", "CLOSE", "RENOVATION"]),
+});
+
+/**
+ * Type for Point of Sale
+ */
+export type PointOfSale = z.infer<typeof PointOfSaleSchemas>;
--- src/lib/schemas/products.ts
+++ src/lib/schemas/products.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
-import { IDSchemas, NoEmptyStringSchemas, WithIdSchemas } from "./base";
+import { IDSchemas, NoEmptyStringSchemas } from "./base";
 
 /**
  * Schema for Category
  */
 export const CategorySchemas = z.object({
   name: NoEmptyStringSchemas,
+  description: z.string().optional(),
 });
 
 /**
@@ -18,9 +19,10 @@ export type Category = z.infer<typeof CategorySchemas>;
  */
 export const ProductSchemas = z.object({
   name: NoEmptyStringSchemas,
-  category: IDSchemas,
+  categoryId: IDSchemas,
+  description: z.string().optional(),
   quantity: z.coerce.number().min(0),
-  price: z.coerce.number().min(0),
+  unitPrice: z.coerce.number().min(0),
   commission: z.coerce.number(),
 });
 
@@ -28,106 +30,3 @@ export const ProductSchemas = z.object({
  * Type for Product
  */
 export type Product = z.infer<typeof ProductSchemas>;
-
-/**
- * Schema for Product Quantity
- */
-export const ProductQuantitySchemas = z.object({
-  product: ProductSchemas,
-  quantity: z.coerce.number().min(0),
-  reason: NoEmptyStringSchemas.optional(),
-});
-
-/**
- * Type for Product Quantity
- */
-export type ProductQuantity = z.infer<typeof ProductQuantitySchemas>;
-
-/**
- * Schema for Point of Sale
- */
-export const PointOfSaleSchemas = z.object({
-  name: NoEmptyStringSchemas,
-  description: z.string().optional(),
-  address: NoEmptyStringSchemas,
-  phoneNumber: NoEmptyStringSchemas,
-  statut: z.enum(["OPEN", "CLOSE", "RENOVATION"]),
-});
-
-/**
- * Type for Point of Sale
- */
-export type PointOfSale = z.infer<typeof PointOfSaleSchemas>;
-
-/**
- * Schema for Client
- */
-export const ClientSchemas = z.object({
-  name: NoEmptyStringSchemas,
-  phoneNumber: z.string().optional(),
-  address: z.string().optional(),
-});
-
-/**
- * Type for Client
- */
-export type Client = z.infer<typeof ClientSchemas>;
-
-/**
- * Schema for Command Item
- */
-export const CommandItemSchemas = z.object({
-  quantity: z.coerce.number().min(1),
-  product: z.object({
-    id: IDSchemas,
-    name: NoEmptyStringSchemas,
-    category: IDSchemas,
-    price: z.coerce.number(),
-  }),
-});
-
-/**
- * Type for Command Item
- */
-export type CommandItem = z.infer<typeof CommandItemSchemas>;
-
-/**
- * Schema for Invoice
- */
-export const InvoiceSchemas = z
-  .object({
-    client: ClientSchemas.merge(WithIdSchemas).nullable(),
-    items: z
-      .array(CommandItemSchemas)
-      .min(1, { message: "Minimum de produit requis" })
-      .superRefine((val, ctx) => {
-        if (val.length !== new Set(val.map((item) => item.product?.id)).size) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: "Produit dupliqué non autorisé.",
-          });
-        }
-      }),
-    saler: IDSchemas,
-  })
-  .refine((schema) => schema.client !== null, {
-    message: "Veiller ajouter le client",
-    path: ["client"],
-  });
-
-/**
- * Type for Invoice
- */
-export type Invoice = z.infer<typeof InvoiceSchemas>;
-
-/**
- * Schema for Items Select
- */
-export const ItemsSelectSchemas = z.object({
-  items: z.array(CommandItemSchemas).min(1),
-});
-
-/**
- * Type for Items Select
- */
-export type ItemsSelect = z.infer<typeof ItemsSelectSchemas>;
