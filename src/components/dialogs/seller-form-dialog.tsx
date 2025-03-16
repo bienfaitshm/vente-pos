e3165a74f1f908ec3ac54e5a1f1f984b78947e13
bienfait shomari <bienfaitshm@gmail.com>
2025-03-16 19:03:32 +0200

    feat: ajouter la gestion de la mise à jour des vendeurs et améliorer le formulaire de création

--- src/components/dialogs/seller-form-dialog.tsx
+++ src/components/dialogs/seller-form-dialog.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import React, { useRef } from "react";
 import {
   Dialog,
   DialogClose,
@@ -9,57 +11,88 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
 import { Button } from "../ui/button";
-import React, { useRef } from "react";
 import { ButtonLoader } from "../button-loader";
 import { useDialogAction } from "@/hooks/dialog-action";
 import { SigninForm } from "../forms/signin-form";
 import { RegistrationCredential } from "@/lib/schemas";
 import { PlusCircle } from "lucide-react";
-import { useDeleteSeller, useSigninUser } from "@/hooks/mutations/accounts";
+import { useSigninUser, useUpdateSeller } from "@/hooks/mutations/accounts";
 
-export type UpdateSelerFormDataType = Pick<
+/**
+ * Type definition for the data used in the seller update form.
+ */
+export type UpdateSelerFormDataType = { id: string } & Pick<
   RegistrationCredential,
   "name" | "email" | "username"
 >;
+
+/**
+ * Interface for the imperative handle of the seller update form dialog.
+ */
 interface SalerUpdateFormDialogRef {
+  /**
+   * Opens the dialog and populates it with the provided data.
+   * @param value - The data to populate the form with.
+   */
   update(value: UpdateSelerFormDataType): void;
 }
 
+/**
+ * Props for the `SalerUpdateFormDialog` component.
+ */
 interface SalerUpdateFormDialogProps {
+  /**
+   * Optional ref to access the imperative handle of the dialog.
+   */
   ref?: React.Ref<SalerUpdateFormDialogRef>;
 }
 
+/**
+ * A dialog component for updating seller information.
+ *
+ * @param props - The props for the component.
+ */
 export const SalerUpdateFormDialog: React.FC<SalerUpdateFormDialogProps> = ({
   ref,
 }) => {
   const btnSubmitRef = React.useRef<HTMLButtonElement>(null);
   const dialogAction = useDialogAction<UpdateSelerFormDataType>();
-  const mutation = useDeleteSeller();
+  const mutation = useUpdateSeller();
+
+  // Expose the `update` method via the imperative handle.
   React.useImperativeHandle(
     ref,
     () => ({
       update(value) {
-        console.log({ value });
         dialogAction.handleOpenDialog(value);
       },
     }),
     [dialogAction]
   );
+
   return (
     <Dialog open={dialogAction.open} onOpenChange={dialogAction.onOpenChange}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Modification</DialogTitle>
           <DialogDescription>
-            Modifier l&apos;élément sélectionné{" "}
+            Modifier l&apos;élément sélectionné
           </DialogDescription>
         </DialogHeader>
         <div>
           <SigninForm
             defaultValues={dialogAction.value}
-            onSubmit={mutation.mutateAsync}
+            onSubmit={({ name, phoneNumber, username, email }) =>
+              mutation.mutateAsync({
+                name,
+                phoneNumber,
+                username,
+                role: "SELLER",
+                email,
+                sellerId: dialogAction.value?.id as string,
+              })
+            }
           >
             <Button type="submit" className="hidden" ref={btnSubmitRef} />
           </SigninForm>
@@ -87,9 +120,13 @@ export const SalerUpdateFormDialog: React.FC<SalerUpdateFormDialogProps> = ({
   );
 };
 
-export const SalerCreateFormDialog: React.FC = ({}) => {
+/**
+ * A dialog component for creating a new seller.
+ */
+export const SalerCreateFormDialog: React.FC = () => {
   const btnSubmitRef = React.useRef<HTMLButtonElement>(null);
   const mutation = useSigninUser();
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -107,7 +144,12 @@ export const SalerCreateFormDialog: React.FC = ({}) => {
           </DialogDescription>
         </DialogHeader>
         <div>
-          <SigninForm showPassword onSubmit={mutation.mutateAsync}>
+          <SigninForm
+            showPassword
+            onSubmit={(data) =>
+              mutation.mutateAsync({ ...data, role: "SELLER" })
+            }
+          >
             <Button type="submit" className="hidden" ref={btnSubmitRef} />
           </SigninForm>
         </div>
@@ -134,6 +176,11 @@ export const SalerCreateFormDialog: React.FC = ({}) => {
   );
 };
 
+/**
+ * Hook to create a ref for the `SalerUpdateFormDialog` component.
+ *
+ * @returns A ref object to control the `SalerUpdateFormDialog`.
+ */
 export function useDeleteSellerFormDialog() {
   return useRef<SalerUpdateFormDialogRef>(null);
 }
