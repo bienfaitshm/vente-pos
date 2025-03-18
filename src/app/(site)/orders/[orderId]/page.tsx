2118697333515394967f32c151260ccafba26c44
bienfait shomari <bienfaitshm@gmail.com>
2025-03-18 04:36:22 +0200

    feat: ajouter des boutons de partage et de téléchargement dans le composant de commande

--- src/app/(site)/orders/[orderId]/page.tsx
+++ src/app/(site)/orders/[orderId]/page.tsx
@@ -1,13 +1,25 @@
 import { PageProps } from "@/app/type";
 import Invoice from "@/components/invoice";
+import { Button } from "@/components/ui/button";
 import { getDefaultStringValue } from "@/lib/formater";
 import { getOrderWithDetails } from "@/server/actions";
+import { DownloadCloudIcon, Share2Icon } from "lucide-react";
 
 export default async function Page({ params }: PageProps<{ orderId: string }>) {
   const { orderId } = await params;
   const order = await getOrderWithDetails({ orderId });
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="max-w-screen-lg mx-auto space-y-5">
+      <div className="flex items-center justify-end gap-4">
+        <Button className="rounded-full">
+          <Share2Icon />
+          <span>Partager</span>
+        </Button>
+        <Button className="rounded-full">
+          <DownloadCloudIcon />
+          <span>Telecharger la facture</span>
+        </Button>
+      </div>
       <div className="p-4 lg:p-8 bg-muted-foreground/10 flex items-center justify-center">
         {order?.data && (
           <Invoice
