c20846ea29ffa8e424e29d3e4c38877899633e07
bienfait shomari <bienfaitshm@gmail.com>
2025-03-28 15:43:28 +0200

    feat(page): ajouter l'affichage des catégories et améliorer la gestion du panier

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,16 +1,29 @@
 import { ProductBagCard } from "@/components/product-bag";
-import { TypographyH2 } from "@/components/ui/typography";
-import { getProducts } from "@/server/actions";
+import { TypographyH2, TypographyH4 } from "@/components/ui/typography";
+import { getCategories, getProducts } from "@/server/actions";
+import { StarIcon } from "lucide-react";
+import Link from "next/link";
 
 
 export default async function Page() {
-    const products = await getProducts({});
-    return <div>
+    const [categories, products] = await Promise.all([await getCategories({}), await getProducts({})]);
+
+    return (<div className="container mx-auto space-y-4 ">
         <TypographyH2>Produits</TypographyH2>
+        <div className="flex gap-4 overflow-x-auto mb-4">
+            {
+                categories?.data?.map(category => (
+                    <Link key={category.id} href={`/?category=${category.id}`} className="flex items-center gap-2 p-3 rounded-xl bg-muted ">
+                        <StarIcon />
+                        <TypographyH4 className="text-lg">{category.name}</TypographyH4>
+                    </Link>
+                ))
+            }
+        </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
             {products?.data?.map(product => (
                 <ProductBagCard key={product.id} id={product.id} productName={product.name} unitPrice={product.unitPrice} />
             ))}
         </div>
-    </div>
+    </div>)
 }
--- src/components/product-bag.tsx
+++ src/components/product-bag.tsx
@@ -6,25 +6,22 @@ import { Button } from "./ui/button"
 
 
 export const ProductBagCard: React.FC<{ id: string, productName: string, unitPrice: number }> = ({ id, productName, unitPrice }) => {
-    const store = useShopingBagStore()
-    const isInBag = store.isIntheBag(id)
-    const toogleBag = () => {
-        if (store.isIntheBag(id)) {
-            store.removeItem(id)
+    const { isIntheBag, addItem, removeItem } = useShopingBagStore()
+    const isInBag = isIntheBag(id)
+
+    const handleBagToggle = () => {
+        if (isInBag) {
+            removeItem(id);
         } else {
-            store.addItem({
-                id,
-                name: productName,
-                unitPrice
-            })
+            addItem({ id, name: productName, unitPrice });
         }
     }
     return (
-        <div className="border p-4 rounded-xl shadow-sm">
+        <div className="p-4">
             <TypographyH4>{productName}</TypographyH4>
             <div>
                 <TypographyH2>{formatCurrency(unitPrice)}</TypographyH2>
-                <Button onClick={toogleBag}>
+                <Button className="w-full rounded-full" onClick={handleBagToggle}>
                     {isInBag ? "Retirer du panier" : "Ajouter au panier"}
                 </Button>
             </div>
