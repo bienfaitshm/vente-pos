874b6c17db2746f79b3751c20ecf62e2b10df6a4
bienfait shomari <bienfaitshm@gmail.com>
2025-03-28 12:10:03 +0200

    feat(page): ajouter une page pour afficher les produits

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,8 @@
+import { TypographyH2 } from "@/components/ui/typography";
+
+
+export default async function Page() {
+    return <div>
+        <TypographyH2>Produits</TypographyH2>
+    </div>
+}
